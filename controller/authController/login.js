01e135970666cd07857790d4b7de0ad821637ecd
Mamc <mamc@Mamcs-MacBook-Pro.local>
2025-09-30 21:53:38 +0530

    change-login

--- controller/authController/login.js
+++ controller/authController/login.js
@@ -15,6 +15,49 @@ export const login = async (req, res) => {
       return res.status(401).json({ message: "Invalid email or password" });
     }
 
+  
+    // generate token
+    const token = jwt.sign(
+      { id: user._id, userRole: user.userRole },
+      process.env.SECRET_JWT,
+      { expiresIn: "7d" }
+    );
+ if(user.userRole === 'admin' || user.userRole === 'gym'){
+  return res.status(403).json({ message: "Access denied. you are not a member" });
+}
+
+    res.status(200).json({
+      message: "Login successful",
+      success: true,
+      token,
+      user: {
+        userId: user._id,
+        name: user.name,
+        email: user.email,
+        userRole: user.userRole
+      }
+    });
+  } catch (error) {
+    console.error("Login Error:", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+export const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email and password are required" });
+    }
+
+    // find user in User collection
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+ if(user.userRole === 'member'){
+  return res.status(403).json({ message: "Access denied. Not an admin user." });
+}
+
    
 
     // generate token
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 
 import express from "express";
 import {  requestOtp, verifyOtp} from "../controller/authController/register.js"
-import { login } from "../controller/authController/login.js";
+import { adminLogin, login } from "../controller/authController/login.js";
 const router = express.Router();
 
 
+router.post("/admin-login", adminLogin);
 router.post("/login", login);
 router.post("/register", requestOtp);
 router.post("/verify-otp", verifyOtp);
